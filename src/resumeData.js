8ffb325a848ad45bc84f886f28848151f9c087db
deepikamadduri <deepikamaddurim@gmail.com>
2020-08-30 23:53:21 +0530

    updated portfolio

---
+++ src/resumeData.js
@@ -0,0 +1,91 @@
+let resumeData = {
+    "name": "Deepika Madduri",
+    "role": "Frontend Developer and Data Scientist",
+    "linkedinId":"https://www.linkedin.com/in/deepika-madduri-55b83b171/",
+    "email":"deepikamaddurim@gmail.com",
+    "roleDescription": "Data Scientist || Frontend Developer || Business Analyst",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/deepika-madduri-55b83b171/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/deepikamadduri",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"An hardworking and ambitious individual with great passion for data science. Possess excellent communication skills and strive hard to achieve the highest standard possible in the work I do. Anexplorer with an enduring motto “ goals above all “ ",
+    "address":"India",
+    "website":"https://github.com/deepikamadduri",
+    "education":[
+      {
+        "UniversityName":"Sri Gowthami smart school",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2015",
+        "Achievements":"Awarded with gold medal for best school magazine project in class 9, Sri Gowthami School,Rajahamundry"
+      },
+      {
+        "UniversityName":"Tirumala Junior College",
+        "specialization":"MPC",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2017",
+        "Achievements":"Awarded Scholarship for my good performance in academics"
+      },
+      {
+        "UniversityName":"Lovely Professional University",
+        "specialization":"Date Science",
+        "MonthOfPassing":"July",
+        "YearOfPassing":"2021",
+        "Achievements":" Qualified the zonal round conducted through a workshop on Machine Learning with python organized by TECHIENEST in association with Aavriti’19 IIT Bombay"
+      }
+    ],
+    "skills":[
+      {
+        "skillname":"DATA SCIENCE"
+      },
+      {
+        "skillname":"MACHINE LEARNING"
+      },
+      {
+        "skillname":"DIGITAL MARKETING"
+      },
+      {
+        "skillname":"PYTHON"
+      },
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Used Cars Analysis",
+        "description":"Analysis of various factors of used cars to check them for resale",
+        "imgurl":"images/cars.jpg"
+      },
+      {
+        "name":"Cafe Management",
+        "description":"Generating an invoice for every customer based on the order number",
+        "imgurl":"images/cafe.jpg"
+      },
+      {
+        "name":"Tourism Website",
+        "description":"Booking of hotel rooms, flights to enjoy their dream destinations ",  
+        "imgurl":"images/tourism.jpg"
+      },
+      {
+        "name":"ATM Management",
+        "description":"Users can do basic transactions, check account balance, Pin change, cash deposit",
+        "imgurl":"images/atm.jpg"
+      }
+    ]
+  }
+  
+  export default resumeData
